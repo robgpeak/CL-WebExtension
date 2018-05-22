2e49ac2ba43a0a842885b17c17db95c00c4b99b8
addisonfreeman <afree1423@gmail.com>
2018-05-22 12:28:49 -0500

    added foxwoods to unbranded branch

--- js/options.js
+++ js/options.js
@@ -250,7 +250,7 @@ var initSubdomain = function() {
 $("#logout").hide();
 var loggedIn = [];
 var userDetail = [];
-var domains = ['shop','xclub','totalrewards'];
+var domains = ['shop','xclub','totalrewards','foxwoods'];
 var recentSubdomain;
 initSubdomain();
 
